ff8fd4bb28cd7a64f79f7a393ad2d0220106ce9b
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2025-07-26 20:54:58 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,11 @@
 from textblob import TextBlob
 import spacy
-
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def analyze_message(text):
     blob = TextBlob(text)
